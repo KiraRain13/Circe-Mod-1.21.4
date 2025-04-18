770c6c101f56fc6211fffcfc226a9da3c382a91e
Arkin Reinier <arkinreinier616@gmail.com>
2025-04-19 00:49:30 +0800

    First item, moly (fix black/purple texture)

--- src/main/java/net/puppeteer/circemod/CirceMod.java
+++ src/main/java/net/puppeteer/circemod/CirceMod.java
@@ -1,5 +1,7 @@
 package net.puppeteer.circemod;
 
+import net.minecraft.world.item.CreativeModeTabs;
+import net.puppeteer.circemod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -36,6 +38,8 @@ public class CirceMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -48,7 +52,9 @@ public class CirceMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.MOLY);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
